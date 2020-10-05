d15969b7fa7bc1c65362b6b37e73f7a75c22d755
Torben Van Assche <torbenvanassche.anime@gmail.com>
2020-10-05 11:16:01 +0200

    fixed animation menu, footstep issues

---
+++ Assets/Scripts/ImageAnimator.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Image))]
+public class ImageAnimator : MonoBehaviour
+{
+    public AnimationContainer sprites;
+    public List<Sprite> animation = new List<Sprite>();
+    private bool firstLoop = true;
+
+    private Image ren = null;
+
+    private float currentTime = 0;
+    private int currentIndex = 0;
+
+    private void Start()
+    {
+        animation.AddRange(sprites.data);
+        
+        ren = GetComponent<Image>();
+        if (sprites)
+        {
+            ren.sprite = sprites.data[currentIndex];   
+        }
+    }
+
+    private void Update()
+    {
+        if (!sprites) return;
+        
+        currentTime += Time.deltaTime;
+
+        if (currentTime >= sprites.animationSpeed)
+        {
+            ++currentIndex;
+            currentTime = 0;
+
+            if (currentIndex >= animation.Count)
+            {
+                currentIndex = 0;
+                if (firstLoop)
+                {
+                    animation.RemoveRange(0, 7);
+                    firstLoop = false;   
+                }
+            }
+
+            ren.sprite = animation[currentIndex];
+        }
+    }
+}
